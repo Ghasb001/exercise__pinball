8f5497c429a39cbfe22b14d87e0965f53d584c44
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-11-29 20:08:47 -0500

    create loading dots animation

--- exercise__pinball/src/App.js
+++ exercise__pinball/src/App.js
@@ -1,6 +1,6 @@
 import logo from './pinball.PNG';
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Coordinates from './coordinates.js';
 import Locations from './locations.js'
 
@@ -8,6 +8,7 @@ function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [near, setNear] = useState([]);
+  const [clicked, setClicked] = useState(false);
 
   // Get the current location based off the browser allowing locations
   const getLocation = async () => {
@@ -27,22 +28,34 @@ function App() {
   };
   /////////////////////////////////////////////////////////////////////////////
 
-  useEffect(() => {
-
-  }, [latitude, longitude])
+  const clickedBool = () => {
+    return clicked ?
+    (<div className="loading">Loading Results</div>) :
+    (<div>Enter Coordinates</div>)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" style={{padding: 20}}/>
-        Enter Coordinates
-        <Coordinates latitude={latitude} longitude={longitude} near={near} setNear={setNear}/>
+        <img src={logo} className="App-logo" alt="logo" style={{ padding: 20 }} />
+        {
+        clickedBool()
+        }
+        <Coordinates latitude={latitude}
+          longitude={longitude}
+          near={near}
+          setNear={setNear}
+          clicked={clicked}
+          setClicked={setClicked}
+        />
         ----------------------
         <button data-testid="near-button" onClick={() => {
+          setClicked(true);
           getLocation()
             .then(({ latitude, longitude }) => {
               setLatitude(latitude);
               setLongitude(longitude);
+              setClicked(false)
             })
             .catch(err => alert('No locations found'));
         }}>Near Me</button>
